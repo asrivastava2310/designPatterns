dd1507cbcd785060169b59641125331ba6e48bce
asrivastava2310 <ashish.srivastav.jss@gmail.com>
2020-05-20 20:04:56 +0530

    Formatting

--- src/designpatterns/behavorial/observer/DeliveryWarehouseCenter.java
+++ src/designpatterns/behavorial/observer/DeliveryWarehouseCenter.java
@@ -3,15 +3,15 @@ package designpatterns.behavorial.observer;
 public class DeliveryWarehouseCenter implements Observer {
 
 	private String location;
-	  
-	  @Override
-	  public void update(String location) {
-	    this.location = location;
-	    showLocation();
-	  }
 
-	  public void showLocation() {
-	    System.out.println("Notification at Warehouse - Current Location: " + location);
-	  }
+	@Override
+	public void update(String location) {
+		this.location = location;
+		showLocation();
+	}
+
+	public void showLocation() {
+		System.out.println("Notification at Warehouse - Current Location: " + location);
+	}
 
 }
--- src/designpatterns/behavorial/observer/Subject.java
+++ src/designpatterns/behavorial/observer/Subject.java
@@ -2,7 +2,9 @@ package designpatterns.behavorial.observer;
 
 //Observables will implement this
 public interface Subject {
-   public void register(Observer o);
-   public void unregister(Observer o);
-   public void notifyObservers();
+
+	public void register(Observer o);
+	public void unregister(Observer o);
+	public void notifyObservers();
+
 }
