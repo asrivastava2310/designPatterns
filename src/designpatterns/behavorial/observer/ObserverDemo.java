02bcccc068402b250a33f688ea9c9a4f904f9c4c
asrivastava2310 <ashish.srivastav.jss@gmail.com>
2020-05-21 12:29:34 +0530

    Formatting

--- src/designpatterns/behavorial/observer/ObserverDemo.java
+++ src/designpatterns/behavorial/observer/ObserverDemo.java
@@ -6,23 +6,23 @@ public class ObserverDemo {
 		DeliveryDataRepo observable = new DeliveryDataRepo();
 		Observer seller = new Seller();
 		Observer customer = new Customer();
-	    Observer deliveryWarehouseCenter = new DeliveryWarehouseCenter();
-	    
-	    //Register observers to observable
-	    observable.register(seller);
-	    observable.register(customer);
-	    observable.register(deliveryWarehouseCenter);
-	    
-	    //make a change
-	    observable.updateLocation("New York");
-	    
-	    //unregister any observer
-	    System.out.println("\nUnregistering Seller");
-	    observable.unregister(seller);
-	    
-	    //make a change again
-	    observable.updateLocation("Chicago");
-	    
+		Observer deliveryWarehouseCenter = new DeliveryWarehouseCenter();
+
+		// Register observers to observable
+		observable.register(seller);
+		observable.register(customer);
+		observable.register(deliveryWarehouseCenter);
+
+		// make a change
+		observable.updateLocation("New York");
+
+		// unregister any observer
+		System.out.println("\nUnregistering Seller");
+		observable.unregister(seller);
+
+		// make a change again
+		observable.updateLocation("Chicago");
+
 	}
 
 }
